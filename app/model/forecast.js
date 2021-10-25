e139453aaa938d846b89b284696e7fd8f4e0402b
gothaw <rad.soltan@gmail.com>
2021-10-25 23:44:42 +0100

    Refactored forecast.js file and added documentation

--- app/model/forecast.js
+++ app/model/forecast.js
@@ -6,43 +6,51 @@ const api = require('../config/api.json');
 const API_KEY = api.WEATHER_API_KEY;
 
 /**
- *
+ * Model for weather forecast. It takes weather forecast from https://www.weatherapi.com using the API_KEY stored in api.json config.
  */
 class Forecast extends EventEmitter {
 
     /**
-     *
-     * @param cityName
+     * Constructor for weather forecast object. It takes city name and makes a request to API.
+     * It uses https get method and parses the data to JSON object when data is received.
+     * @param cityName {String} name of the city to be look
      */
     constructor(cityName) {
         super();
-        let instanceContext = this;
 
         const request = https.get(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${cityName}&aqi=no`, response => {
-            let responseBody = '';
+            this._data = '';
 
             if (response.statusCode !== 200) {
                 request.destroy();
-                instanceContext.emit('error', new Error(`There was an error getting forecast for ${cityName}: (${http.STATUS_CODES[response.statusCode]})`))
+                this.emit('error', new Error(`There was an error getting forecast for ${cityName}: (${http.STATUS_CODES[response.statusCode]})`))
             }
 
             response.on('data', data => {
-                responseBody += data.toString();
-                instanceContext.emit('data', data);
+                this._data += data.toString();
+                this.emit('data', data);
             });
 
             response.on('end', () => {
                 if (response.statusCode === 200) {
                     try {
                         // Parse data
-                        const forecast = JSON.parse(responseBody);
-                        instanceContext.emit('end', forecast)
+                        const forecast = JSON.parse(this._data);
+                        this.emit('end', forecast)
                     } catch (error) {
-                        instanceContext.emit('error', error)
+                        this.emit('error', error)
                     }
                 }
             });
-        }).on('error', error => instanceContext.emit('error', error));
+        }).on('error', error => this.emit('error', error));
+    }
+
+    /**
+     * Getter for the forecast data
+     * @returns {String}
+     */
+    get data() {
+        return this._data;
     }
 }
 
