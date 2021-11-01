7508b834ba8d1777ee7ccbec38ebd87762c8a444
gothaw <rad.soltan@gmail.com>
2021-11-01 22:50:41 +0000

    Added webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const mode = process.env.NODE_ENV || 'development';
+
+const webpack = require("webpack");
+const autoprefixer = require("autoprefixer");
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+
+const optimization = {
+    minimizer: [
+        new TerserPlugin(),
+        new OptimizeCSSAssetsPlugin({})
+    ]
+};
+
+const entry = {
+    'index': [
+        './public/src/scss/style.scss'
+    ]
+};
+
+const _module = {
+    rules: [
+        {
+            test: /\.scss$/,
+            exclude: /node_modules/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: "css-loader",
+                    options: {
+                        url: false
+                    }
+                },
+                "postcss-loader",
+                "sass-loader"
+            ]
+        }
+    ]
+};
+
+const plugins = [
+    new FixStyleOnlyEntriesPlugin(),
+    new MiniCssExtractPlugin({
+        filename: "../public/dist/css/main.css"
+    }),
+    new webpack.LoaderOptionsPlugin({
+        options:{
+            postcss: [
+                autoprefixer()
+            ]
+        }
+    })
+];
+
+module.exports = {
+    optimization,
+    entry,
+    mode,
+    module: _module,
+    plugins
+};
