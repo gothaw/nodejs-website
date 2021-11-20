f4434e3146a42fa108bfbcc8d7f409c8c20da098
gothaw <rad.soltan@gmail.com>
2021-11-20 15:03:43 +0000

    Added logs for dirname

--- app/app.js
+++ app/app.js
@@ -5,9 +5,11 @@ const forecastRoute = require('./api-routes/forecast');
 
 const app = express();
 
-// app.use(express.static(__dirname.replace('app', 'public')));
+app.use(express.static(__dirname.replace('app', 'public')));
+console.log('DIRNAME: ' + __dirname);
 
 app.get('/', (request, response) => {
+    console.log('DIRNAME: ' + __dirname);
     homeRoute.route(request, response);
 });
 
